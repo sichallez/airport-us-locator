8314c4463b3cca471b1c0c74928d5167bbe3885b
sichallez <sich.scidonnees@gmail.com>
2022-11-07 11:44:25 -0500

    netlify doesn't recognize process.cwd

--- pages/api/staticdata.ts
+++ pages/api/staticdata.ts
@@ -3,7 +3,9 @@ import { promises as fs } from "fs";
 
 export default async function handler(req: any, res: any) {
   //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), "json");
+  // const jsonDirectory = path.join(process.cwd(), "json");
+  // Netlify doesn't recognize the path process.cwd(), so have to hard code the path..
+  const jsonDirectory = "json";
   //Read the json data file data.json
   const fileContents = await fs.readFile(
     jsonDirectory + "/us-airports.json",
