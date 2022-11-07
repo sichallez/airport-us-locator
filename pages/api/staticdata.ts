d0cd6c19e2e8b471fe4709a436ca25ae37575be1
sichallez <sich.scidonnees@gmail.com>
2022-11-07 10:34:46 -0500

    fix ts type error

--- pages/api/staticdata.ts
+++ pages/api/staticdata.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import { promises as fs } from "fs";
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   //Find the absolute path of the json directory
   const jsonDirectory = path.join(process.cwd(), "json");
   //Read the json data file data.json
